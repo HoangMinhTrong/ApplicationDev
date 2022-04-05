bb40411aba570b35cda7944be213739a059fbf65
Hoang Minh Trong <minhtrong.hoang@nfq.asia>
2022-04-05 15:12:59 +0700

    first commit

---
+++ ApplicationDev/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace ApplicationDev.Models
+{
+
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
