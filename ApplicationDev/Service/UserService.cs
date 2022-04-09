5ff9833a934654cd9dabbaa3b51f427384b14d40
Hoang Minh Trong <minhtrong.hoang@nfq.asia>
2022-04-09 16:34:41 +0700

    AppDev-003: Add / View Available Roles

---
+++ ApplicationDev/Service/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApplicationDev.Data;
+using ApplicationDev.Service.IService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationDev.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserService(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<List<IdentityRole>> GetAll()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return roles;
+        }
+
+        public async Task<string> AddRole(string roleName)
+        {
+            if (roleName != null)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+            return roleName;
+        }
+    }
+}
